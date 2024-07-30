c30602b85efc9b8960351e76a578b4fb9516ffe3
Your Name <you@example.com>
2024-07-30 17:01:23 +0800

    開始創造用戶資料修改頁面

--- js/roles.js
+++ js/roles.js
@@ -2,137 +2,122 @@ var dataset_rolesList = [
     {
         'id': '1',
         'createDate': "2024-04-25",
-        'company': "系統架構師有限公司",
-        'companyEmail': "abc@gmail.com",
+        'name': "王小明",
+        'userId': "abc@gmail.com",
         'nowLevel': "系統管理員",
-        'status': "1",
-        'uniformNum': "12345678"
+        'status': "啟用",
     },
     {
         'id': '2',
         'createDate': "2011-06-30",
-        'company': "加瑞特·溫特斯貿易公司",
-        'companyEmail': "bcd@gmail.com",
+        'name': "加瑞特·溫特斯貿易公司",
+        'userId': "23456789",
         'nowLevel': "廠商商號",
-        'status': "0",
-        'uniformNum': "23456789"
+        'status': "停用",
     },
     {
         'id': '3',
         'createDate': "2009-01-12",
-        'company': "艾希頓·科克斯股份有限公司",
-        'companyEmail': "cde@gmail.com",
+        'name': "艾希頓·科克斯股份有限公司",
+        'userId': "34567890",
         'nowLevel': "廠商商號",
-        'status': "0",
-        'uniformNum': "34567890"
+        'status': "停用",
     },
     {
         'id': '4',
         'createDate': "2012-03-29",
-        'company': "塞德里克·凱利公司",
-        'companyEmail': "def@gmail.com",
+        'name': "孫小美",
+        'userId': "efg@gmail.com",
         'nowLevel': "系統專員",
-        'status': "0",
-        'uniformNum': "45678901"
+        'status': "停用",
     },
     {
         'id': '5',
         'createDate': "2008-11-28",
-        'company': "愛莉·砂藤貿易有限公司",
+        'name': "愛莉·砂藤貿易有限公司",
         'nowLevel': "廠商商號",
-        'companyEmail': "efg@gmail.com",
-        'status': "1",
-        'uniformNum': "56789012"
+        'userId': "56789012",
+        'status': "啟用",
     },
     {
         'id': '6',
         'createDate': "2012-12-02",
-        'company': "布里耶爾·威廉姆森股份有限公司",
+        'name': "布里耶爾·威廉姆森股份有限公司",
         'nowLevel': "廠商商號",
-        'companyEmail': "fgh@gmail.com",
-        'status': "0",
-        'uniformNum': "67890123"
+        'userId': "67890123",
+        'status': "停用",
     },
     {
         'id': '7',
         'createDate': "2012-08-06",
-        'company': "赫洛德·錢德勒公司",
+        'name': "赫洛德·錢德勒公司",
         'nowLevel': "廠商商號",
-        'companyEmail': "ghi@gmail.com",
-        'status': "1",
-        'uniformNum': "78901234"
+        'userId': "78901234",
+        'status': "啟用",
     },
     {
         'id': '8',
         'createDate': "2010-10-14",
-        'company': "羅娜·戴維森貿易公司",
+        'name': "羅娜·戴維森貿易公司",
         'nowLevel': "廠商商號",
-        'companyEmail': "hij@gmail.com",
-        'status': "1",
-        'uniformNum': "89012345"
+        'userId': "89012345",
+        'status': "啟用",
     },
     {
         'id': '9',
         'createDate': "2009-09-15",
-        'company': "科琳·赫斯特有限公司",
+        'name': "科琳·赫斯特有限公司",
         'nowLevel': "廠商商號",
-        'companyEmail': "ijk@gmail.com",
-        'status': "1",
-        'uniformNum': "90123456"
+        'userId': "90123456",
+        'status': "啟用",
     },
     {
         'id': '10',
         'createDate': "2008-12-13",
-        'company': "索尼婭·弗羅斯特公司",
+        'name': "索尼婭·弗羅斯特公司",
         'nowLevel': "廠商商號",
-        'companyEmail': "jkl@gmail.com",
-        'status': "1",
-        'uniformNum': "01234567"
+        'userId': "01234567",
+        'status': "啟用",
     },
     {
         'id': '11',
         'createDate': "2008-12-19",
-        'company': "珍娜·蓋恩斯股份有限公司",
+        'name': "珍娜·蓋恩斯股份有限公司",
         'nowLevel': "廠商商號",
-        'companyEmail': "klm@gmail.com",
-        'status': "1",
-        'uniformNum': "12345012"
+        'userId': "12345012",
+        'status': "啟用",
     },
     {
         'id': '12',
         'createDate': "2013-03-03",
-        'company': "昆恩·弗林有限公司",
+        'name': "昆恩·弗林有限公司",
         'nowLevel': "廠商商號",
-        'companyEmail': "lmn@gmail.com",
-        'status': "1",
-        'uniformNum': "23456123"
+        'userId': "23456123",
+        'status': "啟用",
     },
     {
         'id': '13',
         'createDate': "2008-10-16",
-        'company': "查爾德·馬歇爾公司",
+        'name': "查爾德·馬歇爾公司",
         'nowLevel': "廠商商號",
-        'companyEmail': "mno@gmail.com",
-        'status': "1",
-        'uniformNum': "34567234"
+        'userId': "34567234",
+        'status': "啟用",
     },
     {
         'id': '14',
         'createDate': "2012-12-18",
-        'company': "海莉·肯尼迪貿易公司",
+        'name': "海莉·肯尼迪貿易公司",
         'nowLevel': "廠商商號",
-        'companyEmail': "nop@gmail.com",
-        'status': "1",
-        'uniformNum': "45678345"
+        'userId': "45678345",
+        'status': "啟用",
     },
     {
         'id': '15',
         'createDate': "2010-03-17",
-        'company': "塔蒂安娜·菲茨帕特里克有限公司",
+        'name': "塔蒂安娜·菲茨帕特里克有限公司",
         'nowLevel': "廠商商號",
-        'companyEmail': "opq@gmail.com",
-        'status': "1",
-        'uniformNum': "56789456"
+        'userId': "56789456",
+        'status': "啟用",
     }
 ];
 
@@ -144,13 +129,12 @@ $(function () {
         ...commonSettingsTable,
         "data": dataset_rolesList,
         "columns": [
-            { data: 'createDate', title: "建立日期", },
-            { data: 'company', title: "公司名稱", },
-            { data: 'companyEmail', title: "E-mail", },
-            { data: 'uniformNum', title: "統一編號", },
-            { data: 'nowLevel', title: "當前角色", },
+            { data: 'createDate', title: "建立日期", }, // 0
+            { data: 'name', title: "使用者", }, // 1
+            { data: 'userId', title: "登入帳號", }, // 2
+            { data: 'nowLevel', title: "當前的權限", }, // 3
             {
-                data: 'nowLevel', title: "調整角色",
+                data: 'nowLevel', title: "調整權限為", // 4
                 render: function (data) {
                     let choose = '';
                     switch (data) {
@@ -168,44 +152,43 @@ $(function () {
                 },
             },
             {
-                data: 'status', title: "啟用",
+                data: 'status', title: "啟用", // 5
                 render: function (data) {
-                    if (data == "1") {
-                        return '<div class="form-check form-switch"><input class="form-check-input" type="checkbox" checked value="1"></div>'
+                    if (data == "啟用") {
+                        return '<div class="form-check form-switch"><input class="form-check-input" type="checkbox" checked value="啟用"></div>'
                     } else {
-                        return '<div class="form-check form-switch"><input class="form-check-input" type="checkbox" value="0"></div>'
+                        return '<div class="form-check form-switch"><input class="form-check-input" type="checkbox" value="停用"></div>'
                     }
                 },
             },
             {
-                data: 'id', title: "儲存",
+                data: 'id', title: "儲存", // 6
                 render: function (data) {
                     return '<button type="button" class="btn btn-outline-primary rounded-circle btn-sm" data-id="' + data + '"><i class="fa-solid fa-floppy-disk"></i></button>'
                 },
             },
         ],
+        order: [[3, 'desc']],
         "columnDefs": [
             {
                 targets: [0],
                 responsivePriority: 3,
             },
             {
-                targets: [4],
+                targets: [1],
                 responsivePriority: 1,
             },
             {
-                targets: [5],
+                targets: [2],
                 responsivePriority: 2,
             },
-            { width: "70px", targets: [6, 7] },
-            { searchable: false, orderable: false, targets: [5, 6, 7] },
-            { className: "text-nowrap", targets: [0, 2, 4] },
-            { className: "text-center", targets: [0, 3, 4, 5, 6, 7] },
+            { searchable: false, orderable: false, targets: [5, 6] },
+            { className: "text-nowrap", targets: [0, 2, 3, 4] },
+            { className: "text-lg-center", targets: [0, 3, 4, 5, 6] },
         ],
         createdRow: function (row, data, dataIndex) {
-            $('td:eq(5)', row).css('min-width', '150px');
-            [0, 4].forEach(function (colIdx) {
-                $('td:eq(' + colIdx + ')', row).css('min-width', '130px');
+            [5, 6].forEach(function (colIdx) {
+                $('td:eq(' + colIdx + ')', row).css('max-width', '70px');
             });
         }
     });
